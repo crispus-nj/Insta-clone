92fed5e014162a57e1492c44e86eb1426f90e067
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-04 16:44:07 +0300

    added two functions two the useraccount models to verify is the user is admin, added styling to my users table in the admin.py file by creatng another class and inheriting userAdmin class

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import UserAccount
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class StyleUserAccount(UserAdmin):
+    list_display = ('username', 'email', 'fullname', 'date_joined', 'last_login', 'is_admin', 'is_active')
+    list_display_links = ('username','email',)
+    list_filter = ()
+    filter_horizontal = ()
+    fieldsets = ()
+    
+
+admin.site.register(UserAccount, StyleUserAccount)
--- instagram/models.py
+++ instagram/models.py
@@ -33,7 +33,7 @@ class UserAccount(AbstractBaseUser):
     username = models.CharField(max_length=50, unique=True)
     fullname = models.CharField(max_length=100)
 
-    USERNAME_FIELDS = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'fullname']
     objects = Account()
 
@@ -46,3 +46,9 @@ class UserAccount(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+
+    def has_module_perms(self, add_label):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return True
