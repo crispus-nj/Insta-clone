4f81fe6d486e4f9a8ee2e600650d3edb9a3a60a7
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-04 16:05:16 +0300

    created login and register templates. styled the templates and added their respective view function

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.login_user, name='login'),
+    path('register/', views.register, name='register'),
 ]
