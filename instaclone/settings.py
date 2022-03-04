38067daa51619853523adf03719536da5ab7802c
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-04 15:13:57 +0300

    added navbar.css file. modified settings.py file by removing secret key and storing the key to the env file, and added env file to gitignore

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4xd(1iqtdnu1khmo^-sk4_@d=s%u&*cjgv*3*jxw5p305+q9kq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
