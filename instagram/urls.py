cd3c7b23a44fa17692a3549b6844429ccbdfc744
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-04 17:40:46 +0300

    created urls.py of the accounts app. added views and restructured the accounts templates

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_user, name='login'),
-    path('register/', views.register, name='register'),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+
+
 @login_required(login_url='login')
 def home(request):
     return render(request, 'instagram/index.html')
 
-def login_user(request):
-    return render(request, 'instagram/login.html')
-def register(request):
-    return render(request, 'instagram/register.html')
