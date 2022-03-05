1a23b7abaa4c740631e52431d7f0a86bb1ccf989
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-06 00:41:30 +0300

    created post_comment function getting the comment and saving the comment to the database. aded the post_comment url with an id to the urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create-post/', views.create_post, name='create-post'),
-    # path('post-comment/<post_id>', views.post_comment, name='comments')
+    path('post-comment/<int:post_id>', views.post_comment, name='comments')
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Comment, Post
 
@@ -17,7 +18,7 @@ def create_post(request):
         description = request.POST.get('description')
 
         post = Post.objects.create(
-            user = request.user,
+            host = request.user,
             image = image,
             description = description
         )
@@ -25,8 +26,23 @@ def create_post(request):
         return redirect('home')
     return render(request, 'instagram/create_post.html')
 
-# @login_required(login_url='login')
-# def post_comment(request, post_id):
-#     print(post_id)
-#     return '(post_id)'
+@login_required(login_url='login')
+def post_comment(request, post_id):
+    post = Post.objects.get(pk = post_id)
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        print(comment)
+        if comment is None:
+            messages.error(request, "Comment can not be empty!")
+        else :
+            comments = Comment.objects.create(
+                user = request.user,
+                post = post,
+                body = comment
+            )
+            # comments.save()
+            return redirect('home')
+    else: 
+        print("invalid data")
+    return render(request, 'instagram/index.html')
 
