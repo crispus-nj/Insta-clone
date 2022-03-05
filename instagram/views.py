4d563cc960de5ad5e1293bf62b340becbd1b242d
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-05 14:41:31 +0300

    created a postform and populated the form in the create_post.html

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
 from .models import Comment, Post
+from .forms import PostForm
 
 @login_required(login_url='login')
 def home(request):
@@ -8,4 +10,13 @@ def home(request):
     comment = Comment.objects.all()
     context = {'post': post, 'comment': comment}
     return render(request, 'instagram/index.html', context)
+
+def create_post(request):
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+    form = PostForm()
+    context = {'form': form}
+    
+    return render(request, 'instagram/create_post.html', context)
 
