88572cc572a20fae1431f377cf8227619dd14f5a
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-05 14:13:46 +0300

    created the card to display all post in the homepage

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Comment, Post
 
 @login_required(login_url='login')
 def home(request):
-    return render(request, 'instagram/index.html')
+    post = Post.objects.all()
+    comment = Comment.objects.all()
+    context = {'post': post, 'comment': comment}
+    return render(request, 'instagram/index.html', context)
 
