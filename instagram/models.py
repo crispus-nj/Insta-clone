f1c65811d8ab526ebf81a071a03e32c221ae3eaf
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-05 13:39:36 +0300

    created the comment and post model. import user model and registered the models to the admin

--- instagram/models.py
+++ instagram/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from accounts.models import UserAccount
+
+
+class Post(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField()
+    description = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    body = models.CharField(max_length=200)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+
+
