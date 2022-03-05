f680c04c7f96ca6003b8c8027c32eee6e87f7111
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-05 13:49:13 +0300

    configured static setting urls, styled the appearance of the post and comment objects

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Comment, Post
+from django.contrib.auth.admin import UserAdmin
 
+class StyledComment(UserAdmin):
+    list_display = ('user', 'post', 'body', 'date_posted')
+    filter_horizontal = ()
+    fieldsets = ()
+    ordering = ('-post',)
+    list_filter = ()
 
+class StylePost(UserAdmin):
+    list_display = ('user', 'image', 'description', 'date_posted')
+    filter_horizontal = ()
+    fieldsets = ()
+    ordering = ('-date_posted',)
+    list_filter = ()
 # Register your models here.
-admin.site.register(Comment)
-admin.site.register(Post)
+admin.site.register(Comment, StyledComment)
+admin.site.register(Post, StylePost)
