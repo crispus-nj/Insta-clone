6249be9359fce147a0d645b8b01f2bab01f78413
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-08 18:34:52 +0300

    modfied settings.py in the email configuration section

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -146,9 +146,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # email configurations
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') 
+EMAIL_PORT = os.environ.get('EMAIL_PORT') 
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 from django.contrib.messages import constants as messages
