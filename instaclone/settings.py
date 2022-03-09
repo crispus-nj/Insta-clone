a74160a49db7867b85eb545c26a48bb19b8b7949
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-09 15:15:34 +0300

    modified the setting.py file to add whitenoise configurations for static files

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -127,6 +127,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
