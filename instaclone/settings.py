8d438b786c6e803f3794feef75080497ce820fcd
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-10 16:30:52 +0300

    added 'django.template.context_processors.media' to the templates in the settings.py file

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+from whitenoise import WhiteNoise
+
+from instaclone import MyWSGIApp
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
