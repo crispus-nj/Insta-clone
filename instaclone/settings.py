be0a0e4d1bed043c857461fb9e345028cbd2c114
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-10 16:36:46 +0300

    removed unused imports in the setting.py file, and an if statement to the url.py file on when to display the images

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-from whitenoise import WhiteNoise
 
-from instaclone import MyWSGIApp
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
