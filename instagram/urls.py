5234eca180315b28c38bb6173897eba381f6b1d5
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-23 17:38:31 +0300

    added like post url path in the urls.py file of the instagram application

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create-post/', views.create_post, name='create-post'),
-    path('post-comment/<int:post_id>', views.post_comment, name='comments')
+    path('post-comment/<int:post_id>', views.post_comment, name='comments'),
+    path('like-post/', views.like_post, name='like'),
 ]
