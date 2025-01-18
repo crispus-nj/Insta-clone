aa403aba59b0a01fda9a1ccb8a99079ea627574a
crispus-n <crispus@thehillcroftgroup.com>
2025-01-18 13:24:58 +0300

    updated allowed host

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -37,7 +37,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://insta-clone-latest.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
